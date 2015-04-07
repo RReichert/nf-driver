1d6ab019b72db17b200c3a0e9f51e228876651ae
Rodrigo Reichert <reichert.rodrigo@gmail.com>
2015-04-08 00:14:47 +1000

    added open functionality

--- src/driver.c
+++ src/driver.c
@@ -8,6 +8,49 @@
 #define DEVICE_NAME "Novint Falcon"
 #define SUCCESS 0
 
+/* LibFTDI CONSTANTS */
+#define SIO_RESET          0
+#define SIO_MODEM_CTRL     1
+#define SIO_SET_FLOW_CTRL  2
+#define SIO_SET_BAUD_RATE  3
+#define SIO_SET_DATA       4
+
+#define SIO_RESET_REQUEST_TYPE  0x40
+#define SIO_RESET_REQUEST       SIO_RESET
+#define SIO_RESET_SIO           0
+#define SIO_RESET_PURGE_RX      1
+#define SIO_RESET_PURGE_TX      2
+
+#define SIO_SET_MODEM_CTRL_REQUEST_TYPE  0x40
+#define SIO_SET_MODEM_CTRL_REQUEST       SIO_MODEM_CTRL
+
+#define SIO_SET_FLOW_CTRL_REQUEST_TYPE   0x40
+#define SIO_SET_FLOW_CTRL_REQUEST        SIO_SET_FLOW_CTRL
+
+#define SIO_SET_BAUDRATE_REQUEST_TYPE    0x40
+#define SIO_SET_BAUDRATE_REQUEST         SIO_SET_BAUD_RATE
+
+#define SIO_SET_DATA_REQUEST_TYPE        0x40
+#define SIO_SET_DATA_REQUEST             SIO_SET_DATA
+
+#define SIO_DISABLE_FLOW_CTRL  0x0
+#define SIO_RTS_CTS_HS        (0x1 << 8)
+#define SIO_DTR_DSR_HS        (0x2 << 8)
+#define SIO_XON_XOFF_HS       (0x4 << 8)
+
+#define SIO_SET_DTR_MASK  0x1
+#define SIO_SET_DTR_HIGH  ( 1 | ( SIO_SET_DTR_MASK  << 8))
+#define SIO_SET_DTR_LOW   ( 0 | ( SIO_SET_DTR_MASK  << 8))
+#define SIO_SET_RTS_MASK  0x2
+#define SIO_SET_RTS_HIGH  ( 2 | ( SIO_SET_RTS_MASK << 8 ))
+#define SIO_SET_RTS_LOW   ( 0 | ( SIO_SET_RTS_MASK << 8 ))
+
+#define SIO_RTS_CTS_HS (0x1 << 8)
+
+#define INTERFACE_ANY 0
+#define INTERFACE_A   1
+#define INTERFACE_B   2
+
 #if defined DEBUG
 #define LOG(format, ...) printk(KERN_ALERT "[*] " DEVICE_NAME ": " format "\n", ##__VA_ARGS__)
 #else
@@ -52,7 +95,7 @@ static struct file_operations device_fops =
 
 static struct usb_class_driver device_class =
 {
-  .name = "usb/falcon%d",
+  .name = "falcon%d",
   .fops = &device_fops
 };
 
@@ -64,9 +107,58 @@ struct device_data
 
 static int device_open(struct inode *inode, struct file *file)
 {
+  // function variables
+  int result;
+  unsigned minor;
+  struct usb_device *device;
+  struct usb_interface *interface;
+
+  // increment module usage counter
   try_module_get(THIS_MODULE);
-  LOG("device open");
+
+  // obtain device details
+  minor = iminor(inode);
+  interface = usb_find_interface(&device_driver, minor);
+  if(!interface) {
+    result = -ENXIO;
+    goto failed_interface;
+  }
+  device = interface_to_usbdev(interface);
+
+  // purge communication
+  if((result = usb_control_msg(device, usb_sndctrlpipe(device,0), SIO_RESET_REQUEST, SIO_RESET_REQUEST_TYPE, SIO_RESET_PURGE_RX, INTERFACE_ANY, NULL, 0, 1e6)) != 0) {
+    goto failed_purge_rx;
+  }
+  if((result = usb_control_msg(device, usb_sndctrlpipe(device,0), SIO_RESET_REQUEST, SIO_RESET_REQUEST_TYPE, SIO_RESET_PURGE_TX, INTERFACE_ANY, NULL, 0, 1e6)) != 0) {
+    goto failed_purge_tx;
+  }
+
+  // setup normal mode
+  if((result = usb_control_msg(device, usb_sndctrlpipe(device,0), 0x09, 0x40, 1, INTERFACE_ANY, NULL, 0, 1e6)) != 0) {
+    goto failed_latency_timer;
+  }
+  if((result = usb_control_msg(device, usb_sndctrlpipe(device,0), SIO_SET_BAUDRATE_REQUEST, SIO_SET_BAUDRATE_REQUEST_TYPE, 0x2, INTERFACE_ANY, NULL, 0, 0)) != 0) {
+    goto failed_baud_rate;
+  }
+  if((result = usb_control_msg(device, usb_sndctrlpipe(device,0), SIO_RESET_REQUEST, SIO_RESET_REQUEST_TYPE, SIO_RESET_PURGE_RX, INTERFACE_ANY, NULL, 0, 1e6)) != 0) {
+    goto failed_purge_rx;
+  }
+  if((result = usb_control_msg(device, usb_sndctrlpipe(device,0), SIO_RESET_REQUEST, SIO_RESET_REQUEST_TYPE, SIO_RESET_PURGE_TX, INTERFACE_ANY, NULL, 0, 1e6)) != 0) {
+    goto failed_purge_tx;
+  }
+
+  // report successfully
   return SUCCESS;
+
+failed_interface:
+failed_purge_tx:
+failed_purge_rx:
+failed_latency_timer:
+failed_baud_rate:
+
+  // report error
+  module_put(THIS_MODULE);
+  return result;
 }
 
 static ssize_t device_read(struct file *file, char __user *user, size_t size, loff_t *offset)
